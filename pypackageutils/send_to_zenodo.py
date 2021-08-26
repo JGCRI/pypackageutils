e2161166fc1281a22adb2ef0cdffb5cc16c75f25
Zarrar Khan <zarrar.khan@pnnl.gov>
2021-08-26 14:11:42 -0400

    adding metadata option to send_to_zenodo

--- pypackageutils/send_to_zenodo.py
+++ pypackageutils/send_to_zenodo.py
@@ -9,11 +9,13 @@ License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 
 import os
 import requests
+import json
 
 
 def send_to_zenodo(access_token = None,
                    delete = False,
-                   id = None):
+                   id = None,
+                   metadata = None):
     """Get the sum from a list of values.
 
     :param access_token:                  Access token to Zenodo Community
@@ -24,6 +26,9 @@ def send_to_zenodo(access_token = None,
 
     :param delete:                  id of Zenodo deposition to edit or delete
     :type delete:                   str
+
+    :param metadata:                 metadata as a python dictionary or path to .csv file
+    :type metadata:                  dict
 
     """
 
@@ -43,6 +48,25 @@ def send_to_zenodo(access_token = None,
         # Get id and doi from temporary upload
         id = str(r.json()["id"])
         doi = str(r.json()["metadata"]["prereserve_doi"]["doi"])
+
+
+    # Add metadata
+    if metadata != None:
+        # If metadata is a path to csv file then read in and convert to dictionary first
+        # Add some checks to make sure metadata entries match allowed keys
+        # If it is a dictionary append the elements to params
+        data = {
+            'metadata': metadata
+        }
+
+        # Now update the metadata for the deposition id
+        r = requests.put('https://zenodo.org/api/deposit/depositions/%s' % id,
+                         params = {'access_token': access_token},
+                         data = json.dumps(data),
+                         headers = headers)
+
+        print(f'Updating deposition id#: {id} with given metadata.')
+        print(r.status_code)
 
     # If delete == TRUE & id != NONE
     if (delete == True) and (id != None):
