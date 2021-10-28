01aeaeaabe1496d455edad44d3211acf4fa1a78f
enlochner <ellie.lochner@pnnl.gov>
2021-10-28 13:12:03 -0400

    Zip folder before upload and update comments

--- pypackageutils/send_to_zenodo.py
+++ pypackageutils/send_to_zenodo.py
@@ -1,90 +1,119 @@
-"""send_to_zenodo  This is function sends up files to the JGCRI community on zenodo.
+""" send_to_zenodo
 
-:author:   Chris R. Vernon
+This script allows users to send up files to the JGCRI community on Zenodo.
+
+:author:   Chris Vernon
 :email:    chris.vernon@pnnl.gov
 
 License:  BSD 2-Clause, see LICENSE and DISCLAIMER files
 
 """
 
-import requests, json, csv
+import requests, json, csv, os, shutil
 
 def upload_zenodo_record(access_token = None,
                          path_to_data = None,
                          metadata = None):
-    """Get the sum from a list of values.
+    """Upload deposition to Zenodo.
 
-    :param access_token:                  Access token to Zenodo Community
-    :type access_token:                   str
+    If data to upload is a directry, this function will zip it first, 
+    otherwise just upload the single file with the correspondeing metadata
+    #Q: can it upload a list of files/ 
 
-    :param path_to_data:                  Path to file or folder to upload
-    :type path_to_data:                   str
+    :param access_token: Access token to Zenodo Community
+    :type access_token: str
 
-    :param metadata:                      metadata as a python dictionary or path to .csv file
-    :type metadata:                       dict
+    :param path_to_data: Path to file or folder to upload
+    :type path_to_data: str
+
+    :param metadata: metadata as a python dictionary or path to .csv file
+    :type metadata: dict or str
 
     """
 
-    # Connect to Zenodo
+    # Connect to Zenodo and create new empty upload
     headers = {"Content-Type": "application/json"}
     params = {'access_token': access_token}
-
-    #Create new upload
     r1 = requests.post('https://zenodo.org/api/deposit/depositions',
                        params=params,
                        json={},
                        headers=headers)
-
+    # Check that upload worked
     if r1.status_code > 210:
         print("Error happened during submission, status code: " + str(r1.status_code))
+        # Print r1.json() to get message as well if it exists?
         return 
 
-    # Get id and doi from temporary upload
+    # Get ID from successful upload
     id = str(r1.json()["id"])
-    doi = str(r1.json()["metadata"]["prereserve_doi"]["doi"])
 
-    # Upload the file and add metadata
+    # Get files to upload
+    if path_to_data is not None: 
+        if os.path.isfile(path_to_data):
+            files = {'file': open(path_to_data, 'rb')} 
+        # If path is a folder, zip it first
+        elif os.path.isdir(path_to_data): 
+            shutil.make_archive(path_to_data, "zip", path_to_data) # Zip
+            files = {'file': open(path_to_data+".zip", 'rb')}
+        else:
+            print("Not valid data")
+            # Delete existing record that we made earlier
+            delete_zenodo_record(access_token, id)
+            return
+    else:
+        print("No data provided")
+        # Delete existing record that we made earlier
+        delete_zenodo_record(access_token, id)
+        return
+
+    # Process metadata if it exists
     if (metadata != None):
-        # If metadata is a path to csv file then read in and convert to dictionary first
-        if type(metadata) == str and "csv" in metadata: #Check if path to csv
-            csv_file = open(metadata, "r") # return a file object that has been opened for reading
-            dict_reader = csv.DictReader(csv_file) # return a csv.DictReader object
-            ordered_dict_from_csv = list(dict_reader)[0] # Convert the csv.DictReader object to a list and extract the first element to return an csv.OrderedDict object
+        # If metadata is a path to csv file, read it in and convert to dictionary
+        if type(metadata) == str and "csv" in metadata: # Check if path to csv
+            csv_file = open(metadata, "r") # Return a file object that has been opened for reading
+            dict_reader = csv.DictReader(csv_file) # Return a csv.DictReader object
+            ordered_dict_from_csv = list(dict_reader)[0] # Convert csv.DictReader object to a list and extract the first element to return an csv.OrderedDict object
             metadata = dict(ordered_dict_from_csv) # Use dict() to convert the csv.OrderedDict object to a dictionary.
 
-            # Append the elements to params
-            data = {
-                'metadata': metadata
-            }
-        elif type(metadata) == dict: 
-            data = {
-                'metadata': metadata
-            } 
-        else: 
+        # Return error if metadata exists but is not a path to a CSV or dictionary 
+        elif type(metadata) != dict: 
             print("Wrong metadata format. Needs to be path to csv or dictionary")
+
+            # Delete existing record that we made earlier
+            delete_zenodo_record(access_token, id)
             return
+
     else: 
-        # Set default metadata
-        data = {
-                "metadata": {'title': 'Untitled', 'description': 'Description.', 'upload_type': 'other'} 
-        } 
+        # If metadata was not provided, set default metadata
+        metadata = {'title': 'Untitled', 'description': 'Description.', 'upload_type': 'other'} 
 
-    # Get files to upload: 
-    if path_to_data is not None: 
-        files = {'file': open(path_to_data, 'rb')}
+    # Append metadata to data object
+    data = {'metadata': metadata}
 
-        # Upload the file or folder
-        r2 = requests.post('https://zenodo.org/api/deposit/depositions/%s/files' % id,
+    # Upload the file or zipped folder
+    r2 = requests.post('https://zenodo.org/api/deposit/depositions/%s/files' % id,
                         params=params,
                         data = data, 
-                        files = files)
-        # put in status code check
+                        files = files)   
+    # Check that upload worked               
+    if r2.status_code > 210:
+        print("Error happened during submission, status code: " + str(r2.status_code))
+        # Delete existing record that we made earlier
+        delete_zenodo_record(access_token, id)   
+        return 
+
+    # TEST UPLOADING MULTIPLE FILES
                         
     # Add metadata
     r3 = requests.put('https://zenodo.org/api/deposit/depositions/%s' % id,
                       params=params, data=json.dumps(data),
                       headers=headers) 
-    # put in status code check
+    # Check that upload worked
+    if r3.status_code > 210:
+        print("Metadata not uploaded correctly, status code: " + str(r3.status_code))
+        # print error message r3.json() w/ it?
+        # Do we want to delete existing record here if the metadat upload part fails?
+        return 
 
     # Create return dictionary
     return_dict = dict()
@@ -102,7 +131,7 @@ def delete_zenodo_record(access_token = None,
     r = requests.delete('https://zenodo.org/api/deposit/depositions/' + id,
                         params=params)
     if r.status_code == 204:
-        print(f'Deleted object with id#: {id}')
+        print(f'Successfully deleted object with id#: {id}')
     #Add error if status code not 204
         return
 
